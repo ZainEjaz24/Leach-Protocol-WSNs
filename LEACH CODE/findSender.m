27a0dac75e392689cf3396ddafb16270e57af17a
Zain <03095956803zain@gmail.com>
2024-02-04 12:24:12 +0500

    Initial commit

---
+++ LEACH CODE/findSender.m
@@ -0,0 +1,23 @@
+% LEACH PROTOCOL FOR WIRELESS SENSOR NETWORKS%
+% IMPLEMENTED BY ZAIN EJAZ (Github ZainEjaz24)
+
+
+%The 'findSender' function is designed to find a sender node based on the
+%Receiver input. %
+
+function Sender=findSender(Sensors,Model,Receiver)
+
+
+    Sender=[];      %Initializes an empty array Sender to store the IDs of potential sender nodes.
+ 
+    n=Model.n;      %nodes
+ 
+    for i=1:n       %loop from 1 to all nodes
+
+        if (Sensors(i).MCH==Receiver && Sensors(i).id~=Receiver)        %verifying sender
+            Sender=[Sender,Sensors(i).id]; %#ok
+        end
+
+    end 
+
+end
---
+++ LEACH CODE/resetSensors.m
@@ -0,0 +1,16 @@
+% LEACH PROTOCOL FOR WIRELESS SENSOR NETWORKS%
+% IMPLEMENTED BY ZAIN EJAZ (Github ZainEjaz24)
+
+%% function is designed to reset certain parameters of the sensor nodes in the network 
+
+function Sensors=resetSensors(Sensors,Model)
+
+
+    n=Model.n;      %nodes
+    for i=1:n                   %loop from 1 to all nodes
+        Sensors(i).MCH=n+1;        %reset sink is member of cluster head
+        Sensors(i).type='N';        % reset all nodes to normal nodes
+        Sensors(i).dis2ch=inf;      %resetting distance to cluster head is infinity
+    end
+    
+end % end of loop
